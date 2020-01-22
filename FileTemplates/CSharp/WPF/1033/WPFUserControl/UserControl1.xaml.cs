2f2e2eb55eca4f90ac966f188f4c958d2a9d2ed7
Novikov Egor <novikov.ea@mail.ru>
2020-01-23 00:11:13 +0300

    File templates were added

---
+++ FileTemplates/CSharp/WPF/1033/WPFUserControl/UserControl1.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace $rootnamespace$
+{
+    public partial class $safeitemrootname$
+    {
+        public $safeitemrootname$()
+        {
+            InitializeComponent();
+        }
+    }
+}
